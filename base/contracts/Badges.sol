fb321e82a22979d2e7a2333b0bb064d9c89c6f26
Sid Raj <siddharth.rajesh13@gmail.com>
2020-12-16 20:32:04 +1100

    added scripts in package.json

--- base/contracts/Badges.sol
+++ base/contracts/Badges.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.7.3;
 
-import "@openzeppelin/contracts/token/ECR721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
