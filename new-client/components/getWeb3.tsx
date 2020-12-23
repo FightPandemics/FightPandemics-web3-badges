bef1146a91a84740ac80acbbb30a13ec3acaec8f
Sid Raj <siddharth.rajesh13@gmail.com>
2020-12-23 18:10:42 +1100

    get contract instance

--- new-client/components/getWeb3.tsx
+++ new-client/components/getWeb3.tsx
@@ -1,11 +1,18 @@
-import { ethers } from "ethers"
+import { ethers, ContractInterface } from "ethers"
 import React, { MouseEvent } from "react"
+import Badges from "../contracts/Badges.json"
 
 export default function GetWeb3(): any {
   async function handleClick(e: MouseEvent) {
     if(typeof window !== "undefined") {
       await (window as any).ethereum.enable()
-      const provider: any = new ethers.providers.Web3Provider((window as any).ethereum);
+      const provider: any = new ethers.providers.Web3Provider((window as any).ethereum)
+      let abi = Badges.abi
+      const contractABI: ContractInterface = new ethers.utils.Interface(abi)
+      const contractAddress: string = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
+      const contractInstance = new ethers.Contract(contractAddress, contractABI, provider)
+      console.log("Contract Instance: ", contractInstance)
 
       console.log("Block number: ", await provider.getBlockNumber())
       e.preventDefault();
@@ -15,6 +22,9 @@ export default function GetWeb3(): any {
 
       let balance: BigInteger = await provider.getBalance(accounts[0])
       console.log("Ether balance: ", ethers.utils.formatEther(balance))
+
+
+
     }
   }
 
