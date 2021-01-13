65d6267646af46615dd3a5c7341cd59cb5b89dce
Sid Raj <siddharth.rajesh13@gmail.com>
2021-01-13 20:02:14 +1100

    fixed axios CORS issue

--- client/src/components/ViewBadge.jsx
+++ client/src/components/ViewBadge.jsx
@@ -1,31 +1,39 @@
 import React from "react";
-import GetProviderMetamask from "../web3/GetProviderMetamask";
-import GetContractRead from "../web3/GetContractRead";
+// import GetProviderMetamask from "../web3/GetProviderMetamask";
+// import GetContractRead from "../web3/GetContractRead";
+import GetContractReadWrite from "../web3/GetContractReadWrite";
+import axios from "axios";
 
 export default function ViewBadge() {
   async function handleClick() {
-    const provider = await GetProviderMetamask();
-    console.log("Provider: ", provider);
+    // const provider = await GetProviderMetamask();
+    // console.log("Provider: ", provider);
 
-    const accounts = await provider.listAccounts();
-    console.log("Accounts: ", accounts);
+    // const accounts = await provider.listAccounts();
+    // console.log("Accounts: ", accounts);
 
-    const contractInstance = await GetContractRead();
+    const contractInstance = await GetContractReadWrite();
     console.log("Contract Instance: ", contractInstance);
 
     const badgeId = (await contractInstance.getLatestId()).toNumber();
+    console.log("Latest Badge ID: ", badgeId);
 
     const tokenURI = await contractInstance.tokenURI(badgeId);
     console.log("TokenURI: ", tokenURI);
 
-    // axios.get(tokenURI, {
-    //   headers: {
-    //     "Access-Control-Allow-Origin": true
-    //   }
-    // })
-    // .then(function (res) {
-    //   console.log("TokenURI Response: ", res)
-    // })
+    const proxyURL = "https://cors-anywhere.herokuapp.com/";
+
+    axios.get(proxyURL + tokenURI, {
+      headers: {
+        "Access-Control-Allow-Origin": true,
+      },
+    })
+      .then(function(res) {
+        console.log("TokenURI Response: ", res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
   return (
     <button onClick={handleClick}>View Badge</button>
