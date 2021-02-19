dc2d74744a029a665997b8eb8b0730c194e4f1e8
Sid Raj <siddharth.rajesh13@gmail.com>
2021-02-19 17:05:39 +1100

    configure webpack

---
+++ api/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./index.js",
+  },
+  target: "node",
+  mode: "production",
+  node: {
+    __filename: false,
+    __dirname: false,
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "commonjs2",
+  },
+  resolve: {
+    extensions: [".js", ".json", ".mjs"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+        },
+        exclude: [
+          /node_modules/,
+          path.resolve(__dirname, "migrations"),
+          path.resolve(__dirname, "seeds"),
+          /knexfile.ts/,
+        ],
+      },
+    ],
+  },
+  externals: {
+    sqlite3: "sqlite3",
+    mysql2: "mysql2",
+    mariasql: "mariasql",
+    mysql: "mysql",
+    oracle: "oracle",
+    "strong-oracle": "strong-oracle",
+    oracledb: "oracledb",
+    "pg-query-stream": "pg-query-stream",
+    mssql: "mssql",
+    tedious: "tedious",
+    "pg-native": "pg-native",
+    "mssql/lib/base": "mssql/lib/base",
+    "mssql/package.json": "mssql/package.json",
+  },
+  plugins: [ new CleanWebpackPlugin() ],
+};
