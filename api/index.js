f0522c7b1b86bf24d59b18c9a375e367e901d655
Sid Raj <siddharth.rajesh13@gmail.com>
2021-02-19 17:45:49 +1100

    set up eslint and babel

--- api/index.js
+++ api/index.js
@@ -1,1 +1,12 @@
-console.log("test");
+import express from "express"
+
+const app = express()
+const PORT = 8000
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
