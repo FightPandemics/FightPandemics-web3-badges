e21e32b4c2bf888cea8d5bd723bed4248dffeec2
Sid Raj <siddharth.rajesh13@gmail.com>
2021-02-27 14:47:29 +1100

    modify migrations to add badge table

--- api/migrations/20210222134357_initial_migration.ts
+++ api/migrations/20210222134357_initial_migration.ts
@@ -11,10 +11,20 @@ export async function up(knex: Knex): Promise<void> {
       table.integer("last_login_at");
       table.timestamps(true, true);
     })
+    .createTable("badge", (table) => {
+      table.increments("id").primary();
+      table.integer("token_id").notNullable();
+      table.integer("clone_from_id").notNullable();
+      table.integer("num_clones_allowed").notNullable();
+      table.integer("num_clones_available").notNullable();
+      table.integer("num_clones_in_wild").notNullable();
+      table.timestamps(true, true);
+    })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
   return knex.schema
-    .dropTableIfExists("users");
+    .dropTableIfExists("user")
+    .dropTableIfExists("badge");
 }
